0e3239208edacadf22d20b20b3eb93c1f2656495
deepthimneni <mikkilinenideepthi@gmail.com>
2020-10-09 18:42:16 +0530

    added views

--- school/urls.py
+++ school/urls.py
@@ -16,3 +16,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from students import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('stud', views.stud),
+    path('show',views.show),
+    path('edit/<int:id>', views.edit),
+    path('update/<int:id>', views.update),
+    path('delete/<int:id>', views.destroy),
+]
